a06f48adb973370989369f24f4956f8db27feefb
Shane Lucy <shanelucy01@protonmail.com>
2021-10-18 20:11:30 +0100

    Formatting change

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
 module.exports = {
   root: true,
-  parser: '@typescript-eslint/parser',
+  parser: "@typescript-eslint/parser",
   extends: [
-    'airbnb-base',
-    'airbnb-typescript/base',
-    'prettier',
-    'plugin:import/recommended'
+    "airbnb-base",
+    "airbnb-typescript/base",
+    "prettier",
+    "plugin:import/recommended",
   ],
-  plugins: ['@typescript-eslint'],
-    parserOptions: {
-    sourceType: 'module',
+  plugins: ["@typescript-eslint"],
+  parserOptions: {
+    sourceType: "module",
     ecmaVersion: 12,
-    project: './tsconfig.json'
+    project: "./tsconfig.json",
   },
   env: {
     browser: true,
     es2021: true,
-    node: true
+    node: true,
   },
 };
